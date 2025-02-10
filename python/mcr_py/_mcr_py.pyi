b3932d978c55a6215eb80ae9af49b08a2e595b2c
Philipp Peter <philipp.peter@wiso.uni-koeln.de>
2025-02-10 08:32:22 +0000

    Make osmtools work

--- python/mcr_py/_mcr_py.pyi
+++ python/mcr_py/_mcr_py.pyi
@@ -2,13 +2,16 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 PyBags = dict[int, list[PyLabel]]
 
+
 class GraphCache:
     def __init__(self) -> None: ...
     def set_graph(self, raw_edges: List[Dict[str, Any]]) -> None: ...
     def set_node_weights(self, node_weights: Dict[int, List[int]]) -> None: ...
     def summary(self) -> None: ...
     def validate_node_id(self, node_id: int) -> None: ...
-    def get_edge_weights(self, start_node_id: int, end_node_id: int) -> List[int]: ...
+    def get_edge_weights(self, start_node_id: int,
+                         end_node_id: int) -> List[int]: ...
+
 
 class PyLabel:
     values: List[int]
@@ -16,8 +19,11 @@ class PyLabel:
     path: List[int]
     node_id: int
 
+
 def run_mlc(graph_cache: GraphCache, start_node_id: int) -> PyBags: ...
 def log_something(): ...
+
+
 def run_mlc_with_node_and_time(
     graph_cache: GraphCache,
     start_node_id: int,
@@ -26,6 +32,8 @@ def run_mlc_with_node_and_time(
     update_label_func: Optional[str] = None,
     enable_limit: Optional[bool] = None,
 ) -> PyBags: ...
+
+
 def run_mlc_with_bags(
     graph_cache: GraphCache,
     bags: Dict[int, List[Union[PyLabel, Any]]],
@@ -33,6 +41,8 @@ def run_mlc_with_bags(
     disable_paths: Optional[bool] = None,
     enable_limit: Optional[bool] = None,
 ) -> PyBags: ...
+
+
 def load_osm_cycling(
     city_name: str,
     bounding_box: List[Tuple[float, float]],
@@ -40,6 +50,8 @@ def load_osm_cycling(
     outpath: str,
     download: bool,
 ): ...
+
+
 def load_osm_walking(
     city_name: str,
     bounding_box: List[Tuple[float, float]],
@@ -47,6 +59,8 @@ def load_osm_walking(
     outpath: str,
     download: bool,
 ): ...
+
+
 def load_osm_driving(
     city_name: str,
     bounding_box: List[Tuple[float, float]],
@@ -54,3 +68,4 @@ def load_osm_driving(
     outpath: str,
     download: bool,
 ): ...
+def download_osm_pbf(target_dir: str): ...
--- src/rs/osmtools_adapter.rs
+++ src/rs/osmtools_adapter.rs
@@ -3,36 +3,42 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 pub fn load_osm_cycling(
+    py: Python,
     city_name: &str,
     geometry_vec: Vec<(f64, f64)>,
     archive_path: &str,
     outpath: &str,
     download: bool,
 ) {
-    println!("Starting load of cycling");
-    _load_osm_cycling(city_name, geometry_vec, archive_path, outpath, download);
+    py.allow_threads(|| {
+        _load_osm_cycling(city_name, geometry_vec, archive_path, outpath, download);
+    })
 }
 
 #[pyfunction]
 pub fn load_osm_driving(
+    py: Python,
     city_name: &str,
     geometry_vec: Vec<(f64, f64)>,
     archive_path: &str,
     outpath: &str,
     download: bool,
 ) {
-    println!("Starting load of driving");
-    _load_osm_driving(city_name, geometry_vec, archive_path, outpath, download);
+    py.allow_threads(|| {
+        _load_osm_driving(city_name, geometry_vec, archive_path, outpath, download);
+    })
 }
 
 #[pyfunction]
 pub fn load_osm_walking(
+    py: Python,
     city_name: &str,
     geometry_vec: Vec<(f64, f64)>,
     archive_path: &str,
     outpath: &str,
     download: bool,
 ) {
-    println!("Starting load of walking");
-    _load_osm_walking(city_name, geometry_vec, archive_path, outpath, download);
+    py.allow_threads(|| {
+        _load_osm_walking(city_name, geometry_vec, archive_path, outpath, download);
+    })
 }
