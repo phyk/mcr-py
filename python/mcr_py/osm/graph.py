15b4bdc037a9f1e019c551d31579b3a4e644f5a5
Philipp K Peter <ppeter1@smail.uni-koeln.de>
2025-04-25 16:29:30 +0000

    Add last tests for osm module

--- python/mcr_py/osm/graph.py
+++ python/mcr_py/osm/graph.py
@@ -9,6 +9,13 @@ from mcr_py.utils.logger import rlog
 def create_rx_graph(
     nodes: pl.DataFrame, edges: pl.DataFrame
 ) -> Tuple[pl.DataFrame, pl.DataFrame, rx.PyDiGraph]:
+    """
+    Creates a directed graph from the given nodes and edges DataFrames.
+
+    :param nodes: pl.DataFrame - A DataFrame containing node information with an 'osm_id' column.
+    :param edges: pl.DataFrame - A DataFrame containing edge information with 'source_osm', 'dest_osm', and 'length' columns.
+    :returns: tuple[pl.DataFrame, pl.DataFrame, rx.PyDiGraph] - A tuple containing the updated nodes DataFrame, updated edges DataFrame, and the constructed directed graph.
+    """
     graph = rx.PyDiGraph()
 
     nodes = nodes.with_columns(
@@ -42,6 +49,14 @@ def create_rx_graph(
 def crop_graph_to_largest_component(
     graph: rx.PyDiGraph, nodes: pl.DataFrame, edges: pl.DataFrame
 ) -> tuple[rx.PyDiGraph, pl.DataFrame, pl.DataFrame]:
+    """
+    Crops the input graph to its largest weakly connected component.
+
+    :param graph: rx.PyDiGraph - The directed graph to be cropped.
+    :param nodes: pl.DataFrame - A DataFrame containing node information with 'rx_node_id' column.
+    :param edges: pl.DataFrame - A DataFrame containing edge information with 'source_rx_node_id' and 'dest_rx_node_id' columns.
+    :returns: tuple[rx.PyDiGraph, pl.DataFrame, pl.DataFrame] - A tuple containing the cropped graph, filtered nodes DataFrame, and filtered edges DataFrame.
+    """
     weakly_connected_components = rx.weakly_connected_components(graph)
     largest_component = max(weakly_connected_components, key=len)
     graph = graph.subgraph(list(largest_component))
@@ -61,26 +76,14 @@ def crop_graph_to_largest_component(
 
 
 def shortest_paths(graph: rx.PyDiGraph, num_threads=4) -> rx.AllPairsPathLengthMapping:
+    """
+    Computes shortest paths for all pairs of nodes in the directed graph using the Bellman-Ford algorithm.
+
+    :param graph: rx.PyDiGraph - The directed graph for which to compute shortest paths.
+    :param num_threads: int - The number of threads to use for parallel processing (default is 4).
+    :returns: rx.AllPairsPathLengthMapping - A mapping of shortest path lengths between all pairs of nodes.
+    """
     os.environ["RAYON_NUM_THREADS"] = str(num_threads)
     return rx.all_pairs_bellman_ford_path_lengths(
         graph, edge_cost_fn=lambda length: length
     )
-
-
-def add_nearest_node_to_stops(
-    stops_df: pl.DataFrame, nx_graph: rx.PyDiGraph
-) -> pl.DataFrame:
-    # osmnx nearest_nodes add -> uses some nx feature
-    # Uses a ckdtree internally
-    # might be good to also do this in osmtools, as the feature is implemented there anyways
-    # -> give a polars dataframe, add columns nearest_node and nearest_node_dist
-    # -> Later, might be important to do this after osm node id reset or before, depending on how the logic is easier
-    nodes, dists = ox.nearest_nodes(
-        nx_graph,
-        stops_df.stop_lon.astype(float),  # TODO: improve this
-        stops_df.stop_lat.astype(float),
-        return_dist=True,
-    )  # type: ignore
-    stops_df["nearest_node"] = nodes
-    stops_df["nearest_node_dist"] = dists
-    return stops_df
