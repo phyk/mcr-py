1a95970962c223dfbf7fc3c680c4916554ffb89e
Philipp K Peter <ppeter1@smail.uni-koeln.de>
2025-08-08 16:11:20 +0200

    Update calculation

--- python/mcr_py/mcr/steps/walking.py
+++ python/mcr_py/mcr/steps/walking.py
@@ -1,12 +1,10 @@
 import polars as pl
+
 from mcr_py import GraphCache
 from mcr_py.mcr.data import (
     TRAVEL_TIME_COLUMN,
-    add_id_column,
     add_weights,
     create_walking_graph,
-    get_reverse_map,
-    reset_node_ids,
     to_mlc_edges,
 )
 from mcr_py.mcr.path import PathType
@@ -30,28 +28,26 @@ class WalkingStepBuilder(StepBuilder):
         osm_edges: pl.DataFrame,
         pois: pl.DataFrame,
     ):
+        osm_nodes = osm_nodes.rename({"rx_node_id": "id"})
+        osm_edges = osm_edges.select(
+            "source_rx_node_id", "dest_rx_node_id", "length"
+        ).rename({"source_rx_node_id": "source_osm", "dest_rx_node_id": "dest_osm"})
         self.walking_nodes, self.walking_edges = create_walking_graph(
             osm_nodes, osm_edges
         )
 
-        self.walking_nodes = add_id_column(self.walking_nodes)
-
-        self.mapping_id_df = self.walking_nodes.select(
-            pl.col(["osm_id", "id"]).unique()
-        )
-        self.walking_node_to_resetted_map = {
-            key: value[0]
-            for key, value in self.mapping_id_df.rows_by_key(
-                key="osm_id", unique=True
-            ).items()
+        from_internal = {
+            key: value
+            for (key, value) in self.walking_nodes.select("id", "osm_id").rows()
         }
-        self.resetted_to_walking_node_map = get_reverse_map(
-            self.walking_node_to_resetted_map
-        )
-
-        self.walking_edges = reset_node_ids(self.walking_edges, self.mapping_id_df)
+        to_internal = {
+            value: key
+            for (key, value) in self.walking_nodes.select("id", "osm_id").rows()
+        }
         pois = pois.join(
-            self.mapping_id_df, left_on="nearest_osm_node", right_on="osm_id"
+            self.walking_nodes.select("id", "osm_id"),
+            left_on="nearest_osm_node",
+            right_on="osm_id",
         ).with_columns(pl.col("id").alias("nearest_osm_node"))
         self.walking_edges = add_weights(self.walking_edges, [TRAVEL_TIME_COLUMN])
         self.walking_edges = add_weights(self.walking_edges, [], hidden=True)
@@ -61,13 +57,14 @@ class WalkingStepBuilder(StepBuilder):
 
         self.walking_graph_cache = GraphCache()
         self.walking_graph_cache.set_graph(raw_walking_edges)  # type: ignore
+
         rlog.debug("Adding POIs to graph")
         self.add_pois_to_walking_graph(pois)
 
         self.kwargs = {
             "graph_cache": self.walking_graph_cache,
-            "to_internal": self.walking_node_to_resetted_map,
-            "from_internal": self.resetted_to_walking_node_map,
+            "from_internal": from_internal,
+            "to_internal": to_internal,
         }
 
     def add_pois_to_walking_graph(self, pois: pl.DataFrame) -> None:
@@ -86,7 +83,6 @@ class WalkingStepBuilder(StepBuilder):
         osm_nodes = osm.list_column_to_osm_nodes(
             self.walking_nodes, pois, "type_internal"
         )
-        rlog.debug("Format of osm nodes: {}".format(osm_nodes.columns))
         resetted_walking_node_id_to_type_map = {
             key: value[0]
             for key, value in osm_nodes.select(
