a778ac96c09f411aab53900e883a4c41ad01d908
Philipp K Peter <ppeter1@smail.uni-koeln.de>
2025-08-29 18:23:22 +0200

    refactor: Simplify labels calculation using polars

--- python/mcr_py/mcr5/labels.py
+++ python/mcr_py/mcr5/labels.py
@@ -1,19 +1,14 @@
-import pathlib
-
 import polars as pl
 
 
-def read_labels_for_nodes(directory: str, nodes: pl.DataFrame) -> pl.DataFrame:
-    base_path = pathlib.Path(directory)
-
+def read_labels_for_nodes(directory: str, nodes: pl.LazyFrame) -> pl.LazyFrame:
     labels = (
-        pl.scan_ipc(base_path.name + "*.feather", include_file_paths="start_id_hex")
+        pl.scan_ipc(directory + "/*.feather", include_file_paths="start_id_hex")
         .join(nodes.lazy(), on="osm_node_id", how="inner")
         .with_columns(
             pl.col("start_id_hex").str.split("/").list.last().str.split(".").list.first(),
             pl.col("osm_node_id").alias("target_id_osm"),
         )
-        .select("start_id_hex", "target_id_osm")
     )
 
-    return labels.collect()
+    return labels
