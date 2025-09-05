bd8aeb75803ad70a321a0185e58a734169f7ac52
Philipp K Peter <ppeter1@smail.uni-koeln.de>
2025-09-05 14:35:36 +0200

    feat(data_pipeline): Unify data pipeline

--- notebooks/01_data_pipeline.py
+++ notebooks/01_data_pipeline.py
@@ -1,8 +1,9 @@
 import datetime
 import io
 import json
-import os
 import pathlib
+import typing
+import zoneinfo
 
 import mcr_py
 import mcr_py.command.area
@@ -13,52 +14,64 @@ import mcr_py.utils.cache
 import mcr_py.utils.geometa
 import mcr_py.utils.logger
 import polars as pl
+import tomllib
 from fsspec.implementations.http import HTTPFileSystem
 
 
-def fetch_gbfs_to_csv(gbfs_url, target_path):
+def fetch_gbfs_to_csv(gbfs_url: str, target_path: pathlib.Path) -> None:
     fs = HTTPFileSystem()
     with fs.open(gbfs_url) as file:
         content = json.load(file)
         df = pl.read_json(io.StringIO(json.dumps(content["data"]["bikes"])))
-    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    target_path.parent.mkdir(parents=True, exist_ok=True)
     df.write_csv(target_path)
 
 
 def load_data_for_city(
-    data_directory,
-    city_name,
-    city_name_german,
-    city_name_german_alt,
-    admin_level,
-    gbfs_url=None,
-):
-    # timestamp = datetime.datetime.today().strftime("%Y%m%d")
-    timestamp = "20250718"
-    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_directory: pathlib.Path,
+    city_name: str,
+    city_name_german: str,
+    city_name_german_alt: str,
+    admin_level: int,
+    crs_sink_name: str,
+    gtfs_timestamp: str,
+    gbfs_url: typing.Union[str, None] = None,
+    timestamp: typing.Union[str, None] = None,
+    now: typing.Union[str, None] = None,
+) -> None:
+    if timestamp is None:
+        timestamp = datetime.datetime.now(tz=zoneinfo.ZoneInfo("Europe/Berlin")).strftime(
+            "%Y%m%d"
+        )
+    if now is None:
+        now = datetime.datetime.now(tz=zoneinfo.ZoneInfo("Europe/Berlin")).strftime(
+            "%Y%m%d-%H%M%S"
+        )
     start_time = "01.01.1970-00:00:00"
     end_time = "01.01.2050-00:00:00"
-    crs_sink_name = "EPSG:4839"
-    gtfs_timestamp = "20250718"
-    cache_path = f"{data_directory}/{timestamp}/cache/"
-    gtfs_path = f"{data_directory}/gtfs_raw/{gtfs_timestamp}/latest.zip"
-    gtfs_crop_path = f"{data_directory}/{timestamp}/gtfs_clean/{city_name}.zip"
-    gtfs_clean_dir = f"{data_directory}/{timestamp}/gtfs_clean/{city_name}/"
-    gtfs_clean_struct = f"{data_directory}/{timestamp}/gtfs_clean/{city_name}/structs.pkl"
-    gbfs_path = f"{data_directory}/{timestamp}/gbfs_raw/{city_name}_{now}.csv"
-    osm_path = f"{data_directory}/{timestamp}/osm_raw"
-    geometa_path = f"{data_directory}/{timestamp}/cache/{city_name}_geometa.pkl"
+    cache_path = data_directory / f"{timestamp}/cache/"
+    gtfs_path = data_directory / f"gtfs_raw/{gtfs_timestamp}/latest.zip"
+    gtfs_crop_path = data_directory / f"{timestamp}/gtfs_clean/{city_name}.zip"
+    gtfs_clean_dir = data_directory / f"{timestamp}/gtfs_clean/{city_name}/"
+    gtfs_clean_struct = data_directory / f"{timestamp}/gtfs_clean/{city_name}/structs.pkl"
+    gbfs_path = data_directory / f"{timestamp}/gbfs_raw/{city_name}_{now}.csv"
+    osm_path = data_directory / f"{timestamp}/osm_raw"
+    geometa_path = data_directory / f"{timestamp}/cache/{city_name}_geometa.pkl"
     mcr_py.utils.logger.setup("INFO")
 
     mcr_py.utils.cache.overwrite_tempdir(cache_path)
     mcr_py.command.area.create_area(
-        city_name_german, admin_level, crs_sink_name, geometa_path=geometa_path
+        city_name_german, admin_level, crs_sink_name, geometa_path=str(geometa_path)
     )
     mcr_py.command.gtfs.gtfs.crop_command(
-        gtfs_path, gtfs_crop_path, start_time, end_time, geometa_path=geometa_path
+        str(gtfs_path),
+        str(gtfs_crop_path),
+        start_time,
+        end_time,
+        geometa_path=geometa_path,
     )
-    mcr_py.command.gtfs.gtfs.clean_gtfs(gtfs_crop_path, gtfs_clean_dir)
-    mcr_py.command.build.build_structures(gtfs_clean_dir, gtfs_clean_struct)
+    mcr_py.command.gtfs.gtfs.clean_gtfs(str(gtfs_crop_path), str(gtfs_clean_dir))
+    mcr_py.command.build.build_structures(str(gtfs_clean_dir), str(gtfs_clean_struct))
     if gbfs_url is not None:
         fetch_gbfs_to_csv(gbfs_url, gbfs_path)
 
@@ -73,20 +86,26 @@ def load_data_for_city(
             mcr_py.mcr.data.NetworkType.CYCLING,
             mcr_py.mcr.data.NetworkType.DRIVING,
         ],
-        redownload=mcr_py.mcr.data.RedownloadMode.OVERWRITE_NOREDOWNLOAD,
+        redownload=mcr_py.mcr.data.RedownloadMode.OVERWRITE_NO_REDOWNLOAD,
     )
-    print("Finished")
+    msg = f"Finished loading data for {city_name_german}"
+    mcr_py.utils.logger.rlog.info(msg)
 
 
-data_path = pathlib.Path(__file__).parent.parent.resolve() / "data"
+if __name__ == "__main__":
+    self_path = pathlib.Path(__file__).parent.resolve()
+    with open(pathlib.Path(__file__).parent.resolve() / "config.toml", "rb") as f:
+        settings = tomllib.load(f)
 
-load_data_for_city(
-    data_path,
-    "cologne",
-    "Köln",
-    "Koeln",
-    6,
-    "https://gbfs.nextbike.net/maps/gbfs/v1/nextbike_kg/de/free_bike_status.json",
-)
-
-load_data_for_city(data_path, "berlin", "Berlin", "Berlin", 4)
+    data_path = pathlib.Path(__file__).parent.parent.resolve() / "data"
+    for city in settings["city"]:
+        load_data_for_city(
+            data_directory=data_path,
+            city_name=city,
+            city_name_german=settings["city"][city]["german"],
+            city_name_german_alt=settings["city"][city]["german_alt"],
+            admin_level=settings["city"][city]["admin_level"],
+            crs_sink_name=settings["geography"]["crs_sink_name"],
+            gtfs_timestamp=settings["gtfs"]["timestamp"],
+            gbfs_url=settings["city"][city]["gbfs_url"],
+        )
