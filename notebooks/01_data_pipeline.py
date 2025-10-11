f63319185faa684aef01efd4cc867d6565f41c08
Philipp K Peter <ppeter1@smail.uni-koeln.de>
2025-10-11 10:47:02 +0200

    fix(data_prep): Fix filename

--- notebooks/01_data_pipeline.py
+++ notebooks/01_data_pipeline.py
@@ -60,7 +60,7 @@ def load_data_for_city(
     gtfs_clean_struct = data_directory / f"{timestamp}/gtfs_clean/{city_name}/structs.pkl"
     gbfs_path = data_directory / f"{timestamp}/gbfs_raw/{city_name}_{now}.parquet"
     osm_path = data_directory / f"{timestamp}/osm_raw"
-    geometa_path = data_directory / f"{timestamp}/cache/{city_name}_geometa.pkl"
+    geometa_path = data_directory / f"{timestamp}/cache/{city_name}_geometa.json"
     time_start_datetime = datetime.datetime.strptime(start_time, "%d.%m.%Y-%H:%M:%S %z")
     time_end_datetime = datetime.datetime.strptime(end_time, "%d.%m.%Y-%H:%M:%S %z")
 
